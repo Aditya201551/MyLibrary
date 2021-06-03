69a35c9b6cc51f8a88de620400684fc6edd159af
Aditya <aditya201551@gmail.com>
2021-06-04 00:39:58 +0530

    routes fixes

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index, name='index'),
-    path('dashboard/',views.dashboard, name='db'),
+    path('account/',views.form,name='form'),
+    # path('dashboard/',views.dashboard, name='db'),
     path('logout/', views.user_logout,name='logout')
 ]
--- app/views.py
+++ app/views.py
@@ -6,7 +6,12 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.urls import reverse
 # Create your views here.
+
 def index(request):
+        return render(request, 'index.html')
+
+
+def form(request):
     if request.method=='POST':
         if request.POST.get('submit')=='Login':
             username = request.POST.get('username')
@@ -16,11 +21,11 @@ def index(request):
             if user:
                 if user.is_active:
                     login(request, user)
-                    return redirect('db')
+                    return redirect('index')
                 else:
-                    return render(request, 'index.html', {'error': 'Account not active contact admin'})
+                    return render(request, 'form.html', {'error': 'Account not active contact admin'})
             else:
-                return render(request, 'index.html', {'error': 'Invalid login credentials'})
+                return render(request, 'form.html', {'error': 'Invalid login credentials'})
         
         elif request.POST.get('submit')=='Sign up':
             username = request.POST['username']
@@ -30,11 +35,9 @@ def index(request):
 
             if password1 == password2:
                 if User.objects.filter(username=username).exists():
-                    # print("USER EXIST")
-                    return render(request, 'index.html', {'error': 'Username already taken'})
+                    return render(request, 'form.html', {'error': 'Username already taken'})
                 elif User.objects.filter(email=email).exists():
-                    return render(request, 'index.html', {'error': 'Email already taken'})
-                    # print("EMAIL ALREADY EXIST")
+                    return render(request, 'form.html', {'error': 'Email already taken'})
                 else:
                     user = User.objects.create_user(
                         username=username, email=email, password=password1)
@@ -43,74 +46,17 @@ def index(request):
                     # print("USER CREATED")
                     user = authenticate(username=username, password=password1)
                     login(request, user)
-                    return redirect('db')
+                    return redirect('index')
             else:
-                # print("INCORRECT PASSWORD")
-                return render(request, 'index.html',  {'error': 'Password not matching'})
+                return render(request, 'form.html',  {'error': 'Password not matching'})
     else:
         if request.user.is_authenticated:
-            return redirect('db')
+            return redirect('index')
         else:
-            return render(request,'index.html')
+            return render(request,'form.html')
 
 
-@login_required(login_url='index')
+@login_required(login_url='form')
 def user_logout(request):
     logout(request)
     return redirect('index')
-
-
-@login_required(login_url='index')
-def dashboard(request):
-        return render(request, 'dashboard.html')
-
-#OLD CODE 
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         username=request.POST.get('username')
-#         password=request.POST.get('password')
-#         user=authenticate(username=username, password=password)
-
-#         if user:
-#             if user.is_active:
-#                 login(request, user)
-#                 return redirect('db')
-#             else:
-#                 return render(request, 'login.html', {'error': 'Account not active contact admin'})
-#         else:
-#             return render(request, 'login.html', {'error': 'Invalid login credentials'})
-#     else:
-#         if request.user.is_authenticated:
-#             return redirect('db')
-#         else:
-#             return redirect('index')
-
-# def user_register(request):
-#     if request.method == 'POST':
-#         username=request.POST['username']
-#         email=request.POST['email']
-#         password1=request.POST['pass']
-#         password2=request.POST['cpass']
-        
-#         if password1==password2:
-#             if User.objects.filter(username=username).exists():
-#                 print("USER EXIST")
-#                 return render(request, 'register.html',{'error':'Username already taken'})
-#             elif User.objects.filter(email=email).exists():
-#                 return render(request, 'register.html', {'error': 'Email already taken'})
-#                 print("EMAIL ALREADY EXIST")
-#             else:
-#                 user=User.objects.create_user(username=username, email=email, password=password1)
-#                 user.set_password(password1)
-#                 user.save()
-#                 print("USER CREATED")
-#                 user = authenticate(username=username, password=password1)
-#                 login(request, user)
-#                 print("fuuuuuuuuuuuuuuuuucccccccccccccccckkkk this bug is just!!!!")
-#                 return redirect(reverse('db', kwargs={"username":username}))
-#         else:
-#             print("INCORRECT PASSWORD")
-#             return render(request, 'register.html',  {'error': 'Password not matching'})
-
-#     return render(request, 'register.html')
